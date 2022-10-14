e5e4904b0c319ec43196c6466b259a0a0482b18d
Elliot Baik <elbaik412@gmail.com>
2022-10-14 18:16:27 -0500

    Add assignMark function

--- script.js
+++ script.js
@@ -1,19 +1,62 @@
-// Gameboard Module with IIFE
+// Gameboard Module with IIFE. Just create board.
 const Gameboard = (() => {
-    // TicTacToe board as array
-    const gameboard = [1,2,3,4,5,6,7,8,9];
+    // TicTacToe board as array with 9 empty elements
+    const gameboard = [,,,,,,,,,];
+
     // DOM stuff
     const selectBoard = document.querySelector('#board');
 
     const createBoard = function() {
         for (let i = 0; i < gameboard.length; i++) {
             const grid = document.createElement('div');
+            grid.setAttribute('id', i);
             grid.classList.add('grid');
+            // grid.addEventListener('click', e => {
+            //     console.log(e.target);
+            //     console.log(e.target.hasChildNodes());
+            // });
+            grid.addEventListener('click', logTarget);
             selectBoard.appendChild(grid);
         };
     };
 
-    const markBoard = function () {
+    function logTarget(e) {
+        console.log(e.target)
+        console.log(typeof e.target.id);
+        console.log(e.target.hasChildNodes());
+        let gridID = e.target.id;
+        return console.log(gridID);
+    };
+
+    const publicBoard = function() {
+        // shows state of board, empty or filled. initially empty
+        console.log(gameboard);
+    };
+
+    return {createBoard, publicBoard, gameboard};
+})();
+
+Gameboard.createBoard();
+Gameboard.publicBoard();
+
+// Player Factory
+const playerFactory = (tagName) => {
+    const getTagName = () => tagName;
+
+    const chooseSide = function () {
+    // what is player playing? X or O? initially undefined
+    let side;
+    let symbol = prompt("Do you want to go first or second?");
+
+        if (symbol !== null && symbol === 'first') {
+            side = 'X';
+            console.log(side);
+        } else if (symbol !== null && symbol === 'second') {
+            side = 'O';
+            console.log(side);
+        }    
+    }
+    const markBoard = function() {
         const gridNodes = document.querySelectorAll('.grid');
         for (let i = 0; i < gridNodes.length; i++) {
             const mark = document.createElement('div');
@@ -22,18 +65,34 @@ const Gameboard = (() => {
             gridNodes[i].appendChild(mark);
         };
     };
-    return {gameboard, createBoard, markBoard};
-})();
 
-Gameboard.createBoard();
-Gameboard.markBoard();
+    const {publicBoard} = Gameboard;
 
-// Player Factory
-const playerFactory = (tagName) => {
-    const sayTagName = () => console.log("Greetings from " + tagName);
-    return {tagName, sayTagName};
+    return {getTagName, chooseSide, publicBoard};
 };
 
 const gorny = playerFactory('Gorny');
 console.log(gorny.tagName);
-console.log(gorny.sayTagName());
+console.log(gorny.getTagName());
+
+// Game State Module
+const GameStateModule = (() => {
+    //keeps track of game state of board
+    const {gameboard} = Gameboard;
+
+    // keeps track of gameboard array, and if a grid is valid (ergo empty)
+    const isValidGrid = function() {
+        // is array index [i] empty?
+    }
+
+    // assign array value index
+    const assignMark = function() {
+        // sample push
+        gameboard[0] = 'O';
+        console.log(gameboard);
+    }
+
+    return {assignMark};
+})();
+
+GameStateModule.assignMark();
