9262fcf497625ec6a8e92a893c40490b0588d3fa
Elliot Baik <elbaik412@gmail.com>
2022-10-21 17:09:08 -0500

    script.js

--- script.js
+++ script.js
@@ -16,10 +16,46 @@ const Gameboard = (() => {
 
 Gameboard.createBoard();
 
+// Now that board is created, create players for use
+// Player Factory creates players
+const playerFactory = (tagName) => {
+
+    const getTagName = () => tagName;
+
+    const chooseSide = function () {
+    // what is player playing? X or O? initially undefined
+    let firstPlayer = 'X';
+    let secondPlayer = 'O';
+
+    let symbol = prompt("Do you want to go first or second?");
+
+        if (symbol !== null && symbol === 'first') {
+            side = 'X';
+            console.log(side);
+        } else if (symbol !== null && symbol === 'second') {
+            side = 'O';
+            console.log(side);
+        } else {
+            console.log("Hey, if you're here, it's time to reset")
+        }
+    }
+
+    return {getTagName, chooseSide};
+};
+
+const gorny = playerFactory('Gorny');
+console.log(gorny.tagName);
+console.log(gorny.getTagName());
+// gorny.chooseSide();
+
 // Game State Module to keep track of board state represented by array
 const GameStateModule = (() => {
     // TicTacToe board as array with 9 empty elements
     const gameboard = [,,,,,,,,,];
+    const gridNodes = document.querySelectorAll('.grid');
+    let count = 0;
+    let isPlayerOneTurn = true;
+    let isPlayerTwoTurn = false;
 
     const addGridListeners = function() {
         // add event listeners to grid divs
@@ -30,25 +66,43 @@ const GameStateModule = (() => {
         });
 
         gridNodes.forEach(grid => {
-            grid.addEventListener('click', updateArrayValue);
+            grid.addEventListener('click', updateGameboardAndGrid);
         })
     };
 
     const isValidGrid = function(e) {
         // keeps track of gameboard array, and if a grid is valid (ergo whether or not it has children)
+        // if valid (false), will mark board
+        // if not (true), will do nothing
         let haveGridChildren = e.target.hasChildNodes();
-        console.log("Does this grid have children? " + haveGridChildren);
-        console.log(haveGridChildren ? "This grid is already taken" : "This grid is free");
+
+        if (haveGridChildren === false) {
+            updateGameboardAndGrid(e);
+        }
     };
 
-    const updateArrayValue = function(e) {
+    const updateGameboardAndGrid = function(e) {
         // update array value index
         let gridID = parseInt(e.target.id);
 
-        if (gameboard[gridID] === undefined) {
-            gameboard[gridID] = gridID;
+        if (gameboard[gridID] === undefined && isPlayerOneTurn === true) {
+            gameboard[gridID] = 'X';
+            markGrid(gridID);
+            turnCounter();
+            publicBoard();
+        } else if (gameboard[gridID] === undefined && isPlayerTwoTurn === true) {
+            gameboard[gridID] = 'O';
+            markGrid(gridID);
+            turnCounter();
             publicBoard();
         };
+    };
+
+    const markGrid = function(num) {
+        const mark = document.createElement('div');
+        mark.classList.add('mark');
+        mark.textContent = gameboard[num];
+        gridNodes[num].appendChild(mark);
     };
 
     const publicBoard = function() {
@@ -56,46 +110,27 @@ const GameStateModule = (() => {
         console.log(gameboard);
     };
 
-    return {addGridListeners, updateArrayValue, isValidGrid};
+    const turnCounter = function() {
+        // increments count variable
+
+        if (count % 2 === 0) {
+            // it's player1 turn :: X
+            // after increment, player turn switches
+            count++;
+            console.log(count);
+            isPlayerOneTurn = false;
+            isPlayerTwoTurn = true;
+        } else {
+            // it's player2 turn :: O
+            count++;
+            console.log(count);
+            isPlayerTwoTurn = false;
+            isPlayerOneTurn = true;
+        }
+    };
+
+    return {addGridListeners, updateGameboardAndGrid, isValidGrid, turnCounter};
 })();
 
 GameStateModule.addGridListeners();
 
-// Now that board is created and kept track of, create players for use
-// Player Factory
-const playerFactory = (tagName) => {
-
-    const getTagName = () => tagName;
-
-    const chooseSide = function () {
-    // what is player playing? X or O? initially undefined
-    let side;
-    let symbol = prompt("Do you want to go first or second?");
-
-        if (symbol !== null && symbol === 'first') {
-            side = 'X';
-            console.log(side);
-        } else if (symbol !== null && symbol === 'second') {
-            side = 'O';
-            console.log(side);
-        }    
-    }
-    const markBoard = function() {
-        const gridNodes = document.querySelectorAll('.grid');
-        for (let i = 0; i < gridNodes.length; i++) {
-            const mark = document.createElement('div');
-            mark.classList.add('mark');
-            mark.textContent = gameboard[i];
-            gridNodes[i].appendChild(mark);
-        };
-    };
-
-    return {getTagName, chooseSide};
-};
-
-const gorny = playerFactory('Gorny');
-console.log(gorny.tagName);
-console.log(gorny.getTagName());
-
-// reorganize code, consider deferring adding event listeners on grids on board creation to a later point
-// click grid and mark symbol
